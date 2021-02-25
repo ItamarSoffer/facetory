326849990d4bee90d72f89072c573263600eeb2b
Itamar Soffer <itamar.soffer@gmail.com>
2021-02-26 01:13:38 +0200

    first commit

---
+++ src/frontend/src/Pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function LoginPage () {
+    return (
+        <div>
+            LoginPage
+        </div>
+    )
+}
---
+++ src/frontend/src/Pages/SlideEditorPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function SlideEditorPage () {
+    return (
+        <div>
+            SlideEditorPage
+        </div>
+    )
+}
