ba518542a2b9e754fc9b706d9c0e953932c6c098
ofirDubi <ofir123dubi@gmail.com>
2021-03-02 15:10:42 +0200

    disable token verification for server mock

--- src/frontend/src/Actions/jwtActions.js
+++ src/frontend/src/Actions/jwtActions.js
@@ -4,10 +4,13 @@ export const checkJwt = (jwtToken) => {
     if (jwtToken === ''){
         return false
     }
+    //console.log("got jwt token!! ", jwtToken)
     const tokenExpiration = jwtDecode(jwtToken).exp;
     const dateNow = new Date();
+    
     // console.log("LEFT", tokenExpiration - dateNow.getTime()/1000);
-    return (tokenExpiration > dateNow.getTime()/1000)
+    //return (tokenExpiration > dateNow.getTime()/1000)
+    return true;
 };
 
 export const refreshByJwt = (jwtToken) => {
