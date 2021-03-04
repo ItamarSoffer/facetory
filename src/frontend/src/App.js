457db40638eba00ed265ae40ba632eb152e2bacf
nadavkandel <nadavkandel@carmel6000.amitnet.org>
2021-03-04 14:57:43 +0200

    imageSearch

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
-import React from 'react';
-import 'antd/dist/antd.css';
+import "./App.css";
+import React from "react";
+import "antd/dist/antd.css";
 import AppRouter from "./Router/AppRouter";
-import 'antd/dist/antd.css';
-import {checkJwt} from "./Actions/jwtActions";
-import { connect } from 'react-redux';
-
+import "antd/dist/antd.css";
+import { checkJwt } from "./Actions/jwtActions";
+import { connect } from "react-redux";
+import ImageSearch from "./Components/imageComponents/imageSearch";
 
 function App(props) {
-  return(
-      <AppRouter
-          isLogged={checkJwt(props.jwtToken)}
-      />
-  )
+  return (
+    // <AppRouter
+    //     isLogged={checkJwt(props.jwtToken)}
+    // />
+    <ImageSearch />
+  );
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-      jwtToken: state.usersReducer.jwtToken,
-      //darkMode: state.sitesReducer.darkMode
-  }
+    jwtToken: state.usersReducer.jwtToken
+    //darkMode: state.sitesReducer.darkMode
+  };
 };
 export default connect(mapStateToProps, null)(App);
