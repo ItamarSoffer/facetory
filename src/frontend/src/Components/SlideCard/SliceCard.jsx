541c0409f300d0f2092c144414e175818a4381d4
unknown <ytzury@gmail.com>
2021-03-04 17:58:34 +0200

    Fix Slice Card

--- src/frontend/src/Components/SlideCard/SliceCard.jsx
+++ src/frontend/src/Components/SlideCard/SliceCard.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Card, Image } from 'antd';
 import Layout, { Content } from 'antd/lib/layout/layout';
+import Meta from 'antd/lib/card/Meta';
+import Avatar from 'antd/lib/avatar/avatar';
 
 const SlideCard = props => {
-    const { imageUrl, cardProps } = props;
-
-    const borderRadius = '10px'
+    const { imageUrl, cardProps, cardStyle} = props;
 
     return (
-            <Card
-                style={{
-                    width: '70%',
-                    margin: '8px',
-                    borderRadius: borderRadius,
-                }}
-                bodyStyle={{
-                    borderRadius: borderRadius,
-                    backgroundImage: `url(${imageUrl})`,
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center center',
-                }}
+      <Card
+          style={{
+              width: '70%',
+              margin:'8px',
+              borderRadius: '10px',
+              backgroundImage: `url(${imageUrl})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
 
-                {...cardProps}
-            >
-              <Layout >
-                <Content style={{ backgroundColor: 'red', height: '100%'}} >
+              position: 'relative',
+              width: '70%',
+              content: "",
+              display: 'block',
+              paddingTop: '50%',
 
-                </Content>
-              </Layout>
-            </Card>
+              ...cardStyle
+          }}
+          {...cardProps}
+      />
     )
 }
 
