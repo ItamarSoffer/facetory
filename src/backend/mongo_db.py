2c76104f6bdcc95c8b5858cf1f9261e60248b69b
eliana <elianag98@gmail.com>
2021-03-04 17:15:10 +0200

    added example

--- src/backend/mongo_db.py
+++ src/backend/mongo_db.py
@@ -3,48 +3,60 @@ from mongoengine import *
 connect("tell_story")
 
 
-class AppUser(Document):
-    user_id = IntField(required=True)
-    name = StringField(max_length=50)
-    password = StringField(max_length=50)
-    stories = ListField()
+class Picture(Document):
+    picture_id = ObjectIdField()
+    path = StringField(required=True)
+    angle = IntField(required=True)
+    coordinat_x = IntField(required=True)
+    coordinat_y = IntField(required=True)
 
 
 class Slide(Document):
-    slide_id = IntField(required=True)
+    slide_id = ObjectIdField()
     background_color = StringField(required=True, max_length=50)
-    background_picture = ObjectIdField
-    pictures = ListField(ObjectIdField)
-    text = StringField(required=True, max_length=50)
-    audio = ObjectIdField
+    background_picture = ReferenceField(Picture)
+    pictures = ListField(ReferenceField(Picture))
+    text = StringField(required=True)
+    audio = StringField(required=True)
 
 
 class Story(Document):
-    story_id = IntField(required=True)
+    story_id = ObjectIdField()
     name = StringField(required=True, max_length=50)
     child_name = StringField(required=True, max_length=50)
     gender = StringField(required=True)
-    slides = ListField(Slide)
+    thumbnail = StringField(required=True)
+    slides = ListField(ReferenceField(Slide))
 
 
-class Pictures(Document):
-    picture_id = ObjectIdField(required=True)
-    path = StringField(required=True, max_length=50)
-    angle = IntField(required=True)
-    cordinate_x = IntField(required=True)
-    cordinate_y = IntField(required=True)
+class AppUser(Document):
+    user_id = ObjectIdField()
+    name = StringField(max_length=50)
+    password = StringField(max_length=50)
+    stories = ListField(ReferenceField(Story))
+
+
+def create_example():
+    pictures = Picture(path='Example.jpg', angle=322, coordinat_x=23, coordinat_y=321)
+    pictures.save()
+    slide = Slide(background_color='#eb34cf', background_picture=pictures, pictures=[pictures], text='hello',
+                  audio='Example.jpg')
+    slide.save()
+    story = Story(name='lefasten', child_name='eliana', gender='girl', thumbnail='Example.jpg', slides=[slide])
+    story.save()
+    app = AppUser(name='eliana', password='pass', stories=[story])
+    app.save()
 
 
 def create_db():
     con = connect("tell_story")
     db = con.get_database("tell_story")
     # db.create_collection("tell_story")
-    # my_user = AppUser(user_id=2)
-    # my_user.save()
+    create_example()
 
 
 def return_user():
-    print(AppUser.objects(user_id=2).count)
+    print(AppUser.objects(user_id=1).count)
 
 
 if __name__ == '__main__':
