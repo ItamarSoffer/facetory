f38c90d134b65f71a0ece2df16b8036bf08c5666
inbar <inbarshoham2@gmail.com>
2021-03-04 17:32:41 +0200

    append to lists when inserting objects

--- src/backend/DAL/Implementation/FacetoryMongoDAL.py
+++ src/backend/DAL/Implementation/FacetoryMongoDAL.py
@@ -14,23 +14,38 @@ class MongoDAL(FacetoryDAL):
         self.host = host
         self.port = port
 
+    def insert_story(self, user_id: int, story_name: str, child_name: str, gender: str):
+        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
+            story = Story(story_name=story_name, child_name=child_name, gender=gender).save()
+            user = AppUser.objects.get(user_id=user_id)
+            user.stories.appand(story)
+            user.save(["stories"])
+
+    def insert_slide(self, user_id: int, story_id: int, background_color: str, background_picture_id: int):
+        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
+            slide = Slide(background_color=background_color, background_picture_id=background_picture_id).save()
+            story = Story.objects.get(story_id=story_id)
+            story.slides.append(slide)
+
     def get_password(self, username: str):
         with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
             return AppUser.objects.get(username=username).password
+
+    def get_slide(self, slide_id: int):
+        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
+            return Slide.objects(slide_id=slide_id)
+
+    def get_story(self, story_id: int):
+        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
+            return Slide.objects(story_id=story_id)
 
     def get_stories(self, user_id: int):
         with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
             return AppUser.objects.get(user_id=user_id).stories
 
-    def insert_story(self, user_id: int, story_name: str, child_name: str, gender: str):
+    def get_slides(self, story_id: int):
         with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
-            story = Story(user_id=user_id, story_name=story_name, child_name=child_name, gender=gender).save()
-        return story
+            return Slide.objects(story_id=story_id)
 
-    def get_slides(self, user_id: int, story_id: int):
-        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
-            return Slide.objects(user_id=user_id, story_id=story_id)
 
-    def get_slide_by_id(self, slide_id: int):
-        with connect(MongoDAL.DB_NAME, host=self.host, port=self.port, alias=MongoDAL.DEFAULT_ALIAS):
-            return Slide.objects(slide_id=slide_id)
+
