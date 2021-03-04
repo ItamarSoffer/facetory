73f544aa3c853c907878971e605553b7e63f91ea
Hillel Fraenkel <hillelfraenkel@carmel6000.amitnet.org>
2021-03-04 11:59:15 +0200

    styles

--- src/frontend/src/Pages/SlideEditorPage.js
+++ src/frontend/src/Pages/SlideEditorPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/slideEditorPage.scss';
+import { Form, Input, Button, Card, Typography } from 'antd';
+const { Title } = Typography;
 
 
-export default function SlideEditorPage () {
+export default function SlideEditorPage() {
     return (
         <div>
+            <Title level={1} style={{ textAlign: "center" }}>Tell Your Story </Title>
             SlideEditorPage
         </div>
     )
