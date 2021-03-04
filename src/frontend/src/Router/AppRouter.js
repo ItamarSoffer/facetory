f5002cabebdeae1edfd2b6f222289ac6ee45a20a
inbar <inbarshoham2@gmail.com>
2021-03-05 01:09:57 +0200

    add menu

--- src/frontend/src/Router/AppRouter.js
+++ src/frontend/src/Router/AppRouter.js
@@ -1,5 +1,9 @@
 import React, {useState} from 'react';
 import {
+    LoginOutlined,
+    RedditOutlined
+} from '@ant-design/icons';
+import {
     BrowserRouter as Router,
     Switch,
     Route,
@@ -13,7 +17,9 @@ import StoryViewerPage from "../Pages/StoryViewerPage";
 import StoryMainPage from "../Pages/StoryMainPage";
 import {FirebaseAuthConsumer, IfFirebaseAuthed, IfFirebaseUnAuthed} from "@react-firebase/auth";
 import firebase from "firebase";
-import {Button} from "antd";
+import {Breadcrumb, Button, Layout, Menu} from "antd";
+import {Header} from "antd/lib/layout/layout";
+import {Content, Footer} from "antd/es/layout/layout";
 
 
 export default function AppRouter(props) {
@@ -23,40 +29,38 @@ export default function AppRouter(props) {
             <Switch>
                 <FirebaseAuthConsumer>
                     {({isSignedIn, user}) => {
-                        return !isSignedIn ? <div>
-                            <Route path="/" component={LoginPage}/>
-                        </div> :
+                        return !isSignedIn ?
                             <div>
-                                <Button type="primary" className="login-form-button" style={{width: '350px', margin: '10px'}} onClick={()=>{
-                                    firebase.auth().signOut();
-                                }}>
-                                    Log Out
-                                </Button>
-                                <Button type="primary" className="login-form-button" style={{width: '350px', margin: '10px'}} onClick={()=>{
-                                    user.getIdToken(true).then((t)=>{setToken(t)})
-                                }}>
-                                    Get token
-                                </Button>
-
-                                {token}
-
-                                <Route path="/story/:story_id/view" exact={true} component={StoryViewerPage}/>
-
-                                <Route path="/story/:story_id/editor" exact={true} component={SlideEditorPage}/>
-
-                                <Route path="/story/:story_id"  exact={true} component={StoryMainPage}/>
-
-                                <Route path="/create" component={CreateStoryPage}/>
-
-                                <Route path="/login" component={LoginPage}/>
-
-                                <Route exact path="/" component={HomePage}/>
+                                <Route path="/" component={LoginPage}/>
+                            </div> :
+                            <div>
+                                <div className="logo"/>
+                                <Menu theme="dark" mode="horizontal" selectable={false}>
+                                    <Menu.Item key="logout" onClick={() => {
+                                        firebase.auth().signOut();
+                                    }}>
+                                        <LoginOutlined/>
+                                    </Menu.Item>
+                                    <RedditOutlined style={{float: "left", fontSize: "32px", marginTop: "5px"}}/>
+                                </Menu>
+                                <Content className="site-layout" style={{padding: '0 50px', marginTop: 64}}>
+                                    <div>
+                                        <Route path="/story/:story_id/view" exact={true}
+                                               component={StoryViewerPage}/>
+                                        <Route path="/story/:story_id/editor" exact={true}
+                                               component={SlideEditorPage}/>
+                                        <Route path="/story/:story_id" exact={true} component={StoryMainPage}/>
+                                        <Route path="/create" component={CreateStoryPage}/>
+                                        <Route path="/login" component={LoginPage}/>
+                                        <Route exact path="/" component={HomePage}/>
+                                    </div>
+                                </Content>
                             </div>
                     }}
-                </FirebaseAuthConsumer>
-            </Switch>
+                        </FirebaseAuthConsumer>
+                        </Switch>
 
-        </Router>
-    )
+                        </Router>
+                        )
 
-}
+                    }
