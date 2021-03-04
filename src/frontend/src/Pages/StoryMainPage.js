3ef15833f4b66e1181f89490cbfb919ea56c1f27
unknown <ytzury@gmail.com>
2021-03-04 14:20:51 +0200

    temp

--- src/frontend/src/Pages/StoryMainPage.js
+++ src/frontend/src/Pages/StoryMainPage.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { Card, Col, Image, Layout, Row } from 'antd';
+import { Content, Header } from 'antd/lib/layout/layout';
+import SlideCard from '../Components/SlideCard/SliceCard';
+
+const { Sider } = Layout;
+
 
 const StoryMainPage = withRouter((props) => {
         return (
-            <div>
-                StoryMainPage
-                {props.history.location.pathname}
-                <button
-                onClick={() =>{props.history.push("/")}}
-                >CLICK</button>
-                <button
-                    onClick={() =>{console.log(props.history.location)}}
-                >LOG</button>
-            </div>
+            <Layout>
+                <Sider
+                style={{
+                    height: '100vh',
+                  }}
+                  >
+                        <Header>
+                            Header
+                        </Header>
+                        <Row
+                            justify="center"
+                        >
+                            <SlideCard cardProps={{onClick: ()=>alert('test')}} imageUrl='https://futurism.com/wp-content/uploads/2015/11/neildegrassetyson.jpg'/>
+                        </Row>
+                </Sider>
+                <Content>
+                  another test
+                </Content>
+            </Layout>
+            // <div>
+            //     StoryMainPage
+            //     {props.history.location.pathname}
+            //     <button
+            //     onClick={() =>{props.history.push("/")}}
+            //     >CLICK</button>
+            //     <button
+            //         onClick={() =>{console.log(props.history.location)}}
+            //     >LOG</button>
+            // </div>
         )
     }
 );
